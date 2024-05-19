7521ef7436225cdf5602f864d913e90f0cb66327
goodsoul-andrew <goodsoul.andrew@mail.ru>
2024-05-19 13:58:57 +0500

    https://pindie.good-backend.nomoredomainswork.ru

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://pindie.good-backend.nomoredomainswork.ru';
+export const BASE_URL = 'https://pindie.good-backend.nomoredomainswork.ru';
 
 export const endpoints = {
 	games: `${BASE_URL}/api/games`,
